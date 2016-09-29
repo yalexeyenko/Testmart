98a5348c6ddc9db24195821e8f3d0649e092759a
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-09-29 16:55:25 +0600

    Create class TestMartPublisher.

---
+++ src/main/java/com/epam/alexeyenko/TestMartPublisher.java
@@ -0,0 +1,9 @@
+package com.epam.alexeyenko;
+
+import javax.xml.ws.Endpoint;
+
+public class TestMartPublisher {
+//	public static void main(String[] args) {
+//		Endpoint.publish("http://localhost:8080/productcatalog", new ProductCatalog());
+//	}
+}
