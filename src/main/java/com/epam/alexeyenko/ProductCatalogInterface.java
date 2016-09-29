737400a7072cdf071df391f11dd71e83bdd3faff
Yevgeniy_Alexeyenko <Yevgeniy_Alexeyenko@.epam.com>
2016-09-29 16:56:40 +0600

    Add attributes to @WebService annotations.

--- src/main/java/com/epam/alexeyenko/ProductCatalogInterface.java
+++ src/main/java/com/epam/alexeyenko/ProductCatalogInterface.java
@@ -3,11 +3,12 @@ package com.epam.alexeyenko;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import com.epam.alexeyenko.model.Product;
 
-@WebService(name = "TestMartCatalog", portName = "TestMartCatalogPort", serviceName = "TestMartCatalogService", targetNamespace = "http://www.testmart.com")
+@WebService(name = "TestMartCatalog", targetNamespace = "http://www.testmart.com")
 public interface ProductCatalogInterface {
 
 	@WebMethod(action = "fetch_categories", operationName = "fetchCategories")
@@ -20,6 +21,7 @@ public interface ProductCatalogInterface {
 	boolean addProduct(String category, String product);
 
 	@WebMethod
+	@WebResult(name = "Product")
 	List<Product> getProductsv2(String category);
 
 }
